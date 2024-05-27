1a4a1e155dce9bb4df7f85466d28dbdea392c026
Stas Polyakov <blckwd69@yandex.ru>
2024-05-28 01:11:52 +0300

    добавил readme и типы

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+//Главная страница
+export interface IAppMain {
+    basket: IBasket[];
+    cardsList: ICard[]
+    preview: string | null;
+    order: IOrder | null;
+  }
+  
+  // Товар
+  export interface ICard {
+    _id: string;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+  }
+  
+  // Данные заказа
+  export interface IOrder {
+    payment: boolean;
+    address: string;
+    email: string;
+    phone: string;
+    cardsId: string[];
+  }
+  
+  // Корзина
+  export interface IBasket {
+    quantity: number;
+    title: string;
+    price: number;
+    totalPrice: number;
+  }
