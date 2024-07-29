262e65fb252e3d6da571df6e77e7064d376f96f3
Stas Polyakov <blckwd69@yandex.ru>
2024-07-30 00:59:13 +0300

    сдача на ревью

---
+++ src/components/AppData.ts
@@ -0,0 +1,131 @@
+import { EventTypes, FormErrors, ICardItem, ILarekApp, IOrder } from "../types";
+import { Model } from "./base/Model";
+
+export type CatalogChangeEvent = {
+  catalog: ICardItem[]
+};
+
+export interface IOrderForm {
+  payment: string;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+export class AppData extends Model<ILarekApp> {
+  basket: ICardItem[] = [];
+  catalog: ICardItem[];
+  order: IOrder = {
+      payment: '',
+      address: '',
+      email: '',
+      phone: '',
+      total: 0,
+      id: [],
+  };
+  preview: string | null;
+  formErrors: FormErrors = {};
+
+  // добавить товар в корзину
+  addBasket(value: ICardItem) {
+    if (!this.basket.some(item => item.id === value.id)) {
+      this.basket.push(value);
+      this.emitChanges(EventTypes.BASKET_CHANGED, this.basket);
+    }
+  }
+
+  // удалить товар из корзины
+  removeBasket(value: ICardItem) {
+    this.basket = this.basket.filter((item) => item !== value);
+    this.emitChanges(EventTypes.BASKET_CHANGED, this.basket);
+  }
+
+  // очистить корзину
+  clearBasket() {
+    this.basket = [];
+    this.emitChanges(EventTypes.BASKET_CHANGED, this.basket);
+  }
+
+  // сумма всех товаров в корзине
+  getTotalPrice() {
+    return this.basket.reduce((total, item) => total + item.price, 0);
+  }
+
+  // каталог товаров
+  setCatalog(items: ICardItem[]) {
+      this.catalog = items;
+      this.emitChanges(EventTypes.ITEMS_CHANGED, { catalog: this.catalog });
+  }
+
+  // предварительный просмотр
+  setPreview(item: ICardItem) {
+      this.preview = item.id;
+      this.emitChanges(EventTypes.PREVIEW_CHANGED, item);
+  }
+
+  // обновление поля формы
+  setOrderField(field: keyof IOrderForm, value: string ) { 
+    this.order[field] = value; 
+    if (this.validateOrder()) { 
+        this.events.emit(EventTypes.ORDER_READY, this.order); 
+    } 
+  } 
+
+  setContactField(field: keyof IOrderForm, value: string ) { 
+    this.order[field] = value; 
+    if (this.validateContact()) { 
+        this.events.emit(EventTypes.CONTACTS_READY, this.order); 
+    } 
+  } 
+
+  validateOrder() { 
+      const errors: typeof this.formErrors = {}; 
+      if (!this.order.payment) { 
+          errors.payment = 'Необходимо выбрать способ оплаты'; 
+      } 
+      if (!this.order.address) { 
+          errors.address = 'Необходимо указать адрес'; 
+      }      
+      this.formErrors = errors; 
+      this.events.emit(EventTypes.FORM_ERRORS_CHANGE, this.formErrors); 
+      return Object.keys(errors).length === 0; 
+  } 
+
+  validateContact() { 
+    const errors: typeof this.formErrors = {}; 
+    if (!this.order.email) { 
+        errors.email = 'Необходимо указать email'; 
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(this.order.email)) {
+        errors.email = 'email должен быть в формате example123@example.example';
+    } 
+    if (!this.order.phone) { 
+        errors.phone = 'Необходимо указать телефон'; 
+    } else if (!/^\+?[0-9]{7,14}$/.test(this.order.phone)) {
+        errors.phone = 'Номер телефона может начинаться с + и состоять толко из цифр';
+    } 
+    this.formErrors = errors; 
+    this.events.emit(EventTypes.FORM_ERRORS_CHANGE, this.formErrors); 
+    return Object.keys(errors).length === 0; 
+  } 
+
+  // очистить форму заказа
+  resetOrderForm() {
+		this.order = {
+			payment: '',
+			address: '',
+			email: '',
+			phone: '',
+			total: 0,
+			id: [],
+		};
+	}
+
+  // данные для заказа
+  orderData() {
+    this.order.id = [];
+    this.basket.forEach((item) => {
+      this.order.id.push(item.id);
+    });
+    this.order.total = this.getTotalPrice();
+  }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,34 +1,56 @@
-//Главная страница
-export interface IAppMain {
-    basket: IBasket[];
-    cardsList: ICard[]
-    preview: string | null;
-    order: IOrder | null;
-  }
-  
-  // Товар
-  export interface ICard {
-    _id: string;
-    description: string;
-    image: string;
-    title: string;
-    category: string;
-    price: number | null;
-  }
-  
-  // Данные заказа
-  export interface IOrder {
-    payment: boolean;
-    address: string;
-    email: string;
-    phone: string;
-    cardsId: string[];
-  }
-  
-  // Корзина
-  export interface IBasket {
-    quantity: number;
-    title: string;
-    price: number;
-    totalPrice: number;
-  }
+// Приложение
+export interface ILarekApp {
+  basket: IBasket[];
+  cardsList: ICardItem[];
+  preview: string | null;
+  order: IOrder | null;
+}
+
+// Товар
+export interface ICardItem {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+  inBasket: boolean;
+}
+
+// Данные заказа
+export interface IOrder {
+  payment: string;
+  address: string;
+  email: string;
+  phone: string;
+  total: number;
+  id: string[];
+}
+
+// Корзина
+export interface IBasket {
+  quantity: number;
+  title: string;
+  price: number;
+  totalPrice: number;
+}
+
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+export enum EventTypes {
+  ITEMS_CHANGED = 'items:changed',
+  CARD_SELECT = 'card:select',
+  PREVIEW_CHANGED = 'preview:changed',
+  BASKET_ADD = 'basket:add',
+  PRODUCT_DELETE = 'product:delete',
+  BASKET_CHANGED = 'basket:changed',
+  BASKET_OPEN = 'basket:open',
+  ORDER_OPEN = 'order:open',
+  FORM_ERRORS_CHANGE = 'formErrors:change',
+  ORDER_READY = 'order:ready',
+  ORDER_SUBMIT = 'order:submit',
+  CONTACTS_READY = 'contacts:ready',
+  CONTACTS_SUBMIT = 'contacts:submit',
+  MODAL_OPEN = 'modal:open',
+  MODAL_CLOSE = 'modal:close',
+}
